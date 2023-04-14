430e310e11e07a8c8ec4a8a48ff656bc9e49b84c
kk1089 <kk1089@mail.ru>
2023-04-14 19:00:38 +0700

    Функции 2.37

--- js/functions.js
+++ js/functions.js
@@ -1,65 +1,69 @@
-// Полинддром (нашел в интернете легко и понятно)
+// Полинддром 
 function palindrome(str) {
-    str = str.toLowerCase().replace(/\s|[,.!?"/-]/g, "");
-    return str.split('').reverse().join('') == str;
+  const newStr = str.toLowerCase().replace(/\s|[,.!?"/-]/g, "");
+  return newStr.split('').reverse().join('') == str;
+}
+
+
+// Длина строки 
+function checkStringLength(lineLength, maxLength) {
+
+  if (lineLength.length <= maxLength) {
+    return true
+  } else {
+    return false
   }
-  /* .replase -  заменяет на то что в скобках
-     .split - разбивает строку на массив
-     .reverse - переворачивает массив
-     .join обьединяет массив 
-     == приравниеваем */
-  
- console.log(palindrome('1232'))
- 
-// Длина строки (сдлелал сам, легко и понятно)
- function getLine (lineLenght, maxLenght) {
-        if (lineLenght.length  <= maxLenght) {
-        console.log(lineLenght.length)
-        } else {
-           console.log('The string cannot exceed ' + maxLenght + ' symbols' )}
-        }
- /* Функция, которая принимает строку,
-  извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в 
-  виде целого положительного числа. 
-  Если в строке нет ни одной цифры, 
-  функция должна вернуть NaN (ничего не понятно)*/
-  function fetchDigits(text) {
+}
+/* Функция, которая принимает строку,
+ извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в 
+ виде целого положительного числа. 
+ Если в строке нет ни одной цифры, 
+ функция должна вернуть NaN */
+function fetchDigits(text) {
   let digits = '';
+
   for (let i = 0; i < text.length; i++) {
-    if (!isNaN(parseInt(text[i], 10))) {
-      digits += text[i];
+
+    if (!isNaN(text[i])) {
+      const digit = parseInt(text[i], 10);
+      digits += digit;
     }
   }
 
   return parseInt(digits, 10);
 }
 
-    /*Функция, которая принимает три параметра: 
-     исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку,
-     дополненную указанными символами до заданной длины.
-     Символы добавляются в начало строки.
-     Если исходная строка превышает заданную длину,
-     она не должна обрезаться.
-     Если «добивка» слишком длинная, она обрезается с конца.  */
-     // Ничего не понятно, но очень интересно
-  function formAddresString (originString, miniLength, addSymbols) {
-   if (originString.length > miniLength) {
-    return originString 
-   }
-   let cyclesCount = miniLength - originString.length;
-   let j = 0;
-   let textBegin = '';
-   for (let i = 0; i < cyclesCount; i++) {
+/*Функция, которая принимает три параметра: 
+ исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку,
+ дополненную указанными символами до заданной длины.
+ Символы добавляются в начало строки.
+ Если исходная строка превышает заданную длину,
+ она не должна обрезаться.
+ Если «добивка» слишком длинная, она обрезается с конца.  */
+// Ничего не понятно, но очень интересно
+function padString(originString, miniLength, addSymbols) {
+
+  if (originString.length > miniLength) {
+
+    return originString
+  }
+  let cyclesCount = miniLength - originString.length;
+  let j = 0;
+  let textBegin = '';
+
+  for (let i = 0; i < cyclesCount; i++) {
     textBegin = textBegin + addSymbols[j];
-    
-    if (j < addSymbols.length -1) {
+
+    if (j < addSymbols.length - 1) {
+
       j++;
+      
     } else {
       originString = textBegin + originString;
       j = 0;
       textBegin = '';
     }
-            
-   }
+
+  }
   return textBegin + originString;
 }
